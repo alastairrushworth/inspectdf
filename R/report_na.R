49e4e33a0b72b267b5d56a58715ef53a69891a58
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2018-11-24 10:40:39 +0000

    report_na option to return df

--- R/report_na.R
+++ R/report_na.R
@@ -1,11 +1,8 @@
 # find columns with missing values
-report_na <- function(df, top_n = 10){
+report_na <- function(df, top_n = 10, type = "df"){
   # perform basic column check on dataframe input
   check_df_cols(df)
-  
-  # print title text
-  console_title("Columns sorted by % missing")
-  
+
   # find the top 10 with most missingness
   df_summary <- vec_to_tibble(sapply(df, sumna)) %>%
     dplyr::mutate(prop = n / nrow(df)) %>%
@@ -13,15 +10,36 @@ report_na <- function(df, top_n = 10){
     dplyr::arrange(desc(prop)) %>%
     dplyr::slice(1:top_n) 
   
-  # if there is anything to print, else sensible message
+  # if any missing values then print out
   if(nrow(df_summary) > 0){
-    df_summary %>% dot_bars_na
+    # print to console
+    if(type == "console"){
+      # print title text
+      console_title("Columns sorted by % missing")
+      # print console chart
+      df_summary %>% dot_bars_na
+      # invisibly return the df for further summaries
+      invisible(df)
+    }
+    # print dfs
+    if(type == "df"){
+      # return dataframe of values
+      return(df_summary)
+    }
   } else {
-    cat(silver("    << Not applicable >>\n"))
+    if(type == "console"){
+      # print title text
+      console_title("Columns sorted by % missing")
+      # print console chart
+      cat(silver("    << Not applicable >>\n"))
+      # invisibly return the df for further summaries
+      invisible(df)
+    } 
+    if(type == "df"){
+      # return dataframe of values
+      return(tibble(names = character(), n = integer(), value = numeric()))
+    }
   }
-  
-  # invisibly return the df for further summaries
-  invisible(df)
 }
 
 
