51c4d04ede6cccaacead3e9765ae848e2e1e4ea2
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2018-12-02 11:07:01 +0000

    Help file for the report function

--- R/report.R
+++ R/report.R
@@ -1,3 +1,13 @@
+#' Report summary statistics for a data frame 
+#'
+#' @param df A data frame
+#' @param top_n The number of rows to print for summaries \code{report_na}, \code{report_cor}, \code{report_imbalance}, \code{report_association}, Default \code{NULL} prints everything.
+#' @return Prints statistics to the console.
+#' @details \code{report} is a wrapper for the all of the individual reporting functions \code{report_space}, \code{report_types}, \code{report_na}, \code{report_cor}, \code{report_imbalance}, \code{report_association}.
+#' @examples
+#' report(mtcars)
+
+
 report <- function(df, top_n = NULL){
   df %>%
     report_space(top_n = top_n) %>%
