4701539d78a997e56a4ca25c332ae4b9021e5d39
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2018-12-02 12:06:21 +0000

    Documentation for cor function

--- R/report_cor.R
+++ R/report_cor.R
@@ -1,3 +1,12 @@
+#' Report the Pearson's correlation coefficient for each pair of numeric columns
+#'
+#' @param df A data frame
+#' @param top_n The number of rows to print for summaries. Default \code{top_n = NULL} prints everything.
+#' @param type Character specificying report output type.  Default \code{type = "df"} causes report to be returned as a tibble.   \code{type = "console"} causes report to be returned directly to the console.
+#' @return Return a \code{tibble} containing the columns \code{col_1}, \code{col_2} and \code{pair} and \code{correlation}.  The report contains only the upper triangle of the correlation matrix.  The tibble is sorted by descending absolute value in the \code{correlation} column.
+#' @examples
+#' report_cor(mtcars)
+
 report_cor <- function(df, top_n = NULL, type = "df"){
   
   # perform basic column check on dataframe input
@@ -17,17 +26,17 @@ report_cor <- function(df, top_n = NULL, type = "df"){
     cor_df <- cor_df %>% dplyr::filter(!is.na(cor)) %>%
       dplyr::arrange(desc(abs(cor))) %>%
       dplyr::mutate(pair = paste(X1, X2, sep = " & ")) %>%
-      dplyr::select(X1, X2, pair, cor) 
+      dplyr::select(col_1 = X1, col_2 = X2, pair, correlation = cor) 
     out <- cor_df %>% dplyr::slice(1:min(top_n, nrow(.))) 
     # if user doesn't request dataframe output
     if(type == "console"){
       # print title text
       console_title("Most correlated numeric pairs")
       # print console chart
-      out %>% select(-X1, -X2, cor, pair) %>% dot_bars_cor 
+      out %>% select(-col_1, -col_2, cor = correlation, pair) %>% dot_bars_cor 
     } 
     if(type == "df"){
-      # return dataframe of 
+      # return dataframe of correlations
       return(out)
     }
   } else {
@@ -39,8 +48,8 @@ report_cor <- function(df, top_n = NULL, type = "df"){
     } 
     if(type == "df"){
       # return empty dataframe of 
-      return(tibble(X1 = character(), X2 = character(), 
-                    pair = character(), cor = numeric()))
+      return(tibble(col_1 = character(), col_2 = character(), 
+                    pair = character(), correlation = numeric()))
     }
   }
   if(type == "console") invisible(df)
