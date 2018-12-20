8052f56c7af1fe286c6879f6f0e588aa2ee6b6b5
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2018-12-20 07:42:23 +0000

    Edit to ensure report_imbalance does not fail when pair of dfs are identical

--- R/prop_test.R
+++ R/prop_test.R
@@ -1,10 +1,12 @@
 prop_test <- function(na_1, na_2, n_1, n_2){
   p_value <- vector("numeric", length = length(na_1))
-  for(i in 1:length(p_value)){
-    if((is.na(na_1[i]) | is.na(na_2[i])) | (na_1[i] == 0 & na_2[i] == 0)){
-      p_value[i] <- NA
-    } else {
-      p_value[i] <- suppressWarnings(prop.test(c(na_1[i], na_2[i]), c(n_1, n_2))$p.value)
+  if(length(p_value) > 0){
+    for(i in 1:length(p_value)){
+      if((is.na(na_1[i]) | is.na(na_2[i])) | (na_1[i] == 0 & na_2[i] == 0)){
+        p_value[i] <- NA
+      } else {
+        p_value[i] <- suppressWarnings(prop.test(c(na_1[i], na_2[i]), c(n_1, n_2))$p.value)
+      }
     }
   }
   return(p_value)
@@ -15,11 +17,13 @@ prop_test <- function(na_1, na_2, n_1, n_2){
 
 prop_test_imbalance <- function(imbal_tab, n_1, n_2){
   p_value <- vector("numeric", length = nrow(imbal_tab))
-  for(i in 1:nrow(imbal_tab)){
-    if((!(imbal_tab$value_1[i] == imbal_tab$value_2[i])) | (is.na(imbal_tab$percent_1[i])|is.na(imbal_tab$percent_2[i]))){
-      p_value[i] <- NA
-    } else {
-      p_value[i] <- suppressWarnings(prop.test(c(imbal_tab$percent_1[i] * n_1 / 100, imbal_tab$percent_2[i] * n_2 / 100), c(n_1, n_2))$p.value)
+  if(length(p_value) > 0){
+    for(i in 1:nrow(imbal_tab)){
+      if((!(imbal_tab$value_1[i] == imbal_tab$value_2[i])) | (is.na(imbal_tab$percent_1[i])|is.na(imbal_tab$percent_2[i]))){
+        p_value[i] <- NA
+      } else {
+        p_value[i] <- suppressWarnings(prop.test(c(imbal_tab$percent_1[i] * n_1 / 100, imbal_tab$percent_2[i] * n_2 / 100), c(n_1, n_2))$p.value)
+      }
     }
   }
   return(p_value)
