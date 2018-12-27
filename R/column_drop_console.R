4461842c2e207c496a175e4a269e1417f856ce38
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2018-12-27 15:42:35 +0000

    removals

--- R/column_drop_console.R
+++
@@ -1,6 +0,0 @@
-column_drop_console <- function(type, names_to_drop = NULL){
-  if(is.null(names_to_drop)) names_to_drop <- silver("<none>")
-  cat(paste(type, " ", sep = ""))
-  cat(paste("\U2022 ", names_to_drop, sep = ""))
-  cat("\n")
-}
