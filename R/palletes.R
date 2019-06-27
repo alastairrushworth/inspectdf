ac0c8a056e9c663478bf5c0f74354f7f8591e6bf
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2019-06-27 08:18:20 +0100

    update to rainbow palette

--- R/palletes.R
+++ R/palletes.R
@@ -7,12 +7,12 @@ user_colours <- function(n, i){
     palette_list <- list(
       cbfriendly = c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"),
       `80s` = c("#ff48c4", "#2bd1fc", "#f3ea5f", "#c04df9", "#ff3f3f"),
-      rainbow = c("#e40303", "#f27630", "#ffed00", "#008026",	"#004dff"),
+      rainbow = c("#e70000", "#ff8c00", "#ffef00", "#00811f",	"#0044ff", "#760089"),
       mario = c("#fed1b0", "#ee1c25", "#0065b3", "#ffffff", "#894c2f"), 
       pokemon = c("#b3ffa9", "#ffa9b3",	"#a9b3ff", "#fdff98", "#dfc189")
     )
     if(i == 3){
-      colour_vector <- rep(palette_list, length.out = n)
+      colour_vector <- rep(palette_list[[i]], length.out = n)
     } else {
       x <- colorRampPalette(palette_list[[i]])
       colour_vector <- x(n)
