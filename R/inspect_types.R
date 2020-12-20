422c02579bc5180f3978ca0fbfe8f5a63f2800d0
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2020-12-20 14:01:00 +0000

    More detailed reports on type comparison

--- R/inspect_types.R
+++ R/inspect_types.R
@@ -5,6 +5,8 @@
 #'
 #' @param df1 A dataframe.
 #' @param df2 An optional second dataframe for comparison.  
+#' @param compare_index Whether to check column positions as well as types when comparing dataframes.  
+#' Defaults to \code{FALSE}.
 #' @return A tibble summarising the count and percentage of different 
 #' column types for one or a pair of data frames.
 #' @details 
@@ -60,7 +62,7 @@
 #' @importFrom tidyr replace_na
 #' @useDynLib inspectdf
 
-inspect_types <- function(df1, df2 = NULL){
+inspect_types <- function(df1, df2 = NULL, compare_index = FALSE){
   
   # perform basic column check on dataframe input
   check_df_cols(df1)
@@ -99,20 +101,78 @@ inspect_types <- function(df1, df2 = NULL){
     attr(out, "type") <- list(method = "types", 1)
     attr(out, "df_names") <- df_names
   } else {
-    s1 <- inspect_types(df1) %>% select(-col_name)
+    # inspect tyoes for first df
+    s1  <- inspect_types(df1)
     colnames(s1)[2:3] <- paste0(c("cnt_", "pcnt_"), 1)
-    s2 <- inspect_types(df2) %>% select(-col_name)
+    # inspect tyoes for second df
+    s2  <- inspect_types(df2)
     colnames(s2)[2:3] <- paste0(c("cnt_", "pcnt_"), 2)
-    out <- full_join(s1, s2, by = "type") %>% 
+    # join together
+    out <- full_join(s1, s2, by = "type") 
+    # create an log column flagging issues 
+    if(compare_index){
+      log_logical <- as.integer(mapply(identical, x = out$col_name.x, y = out$col_name.y)) + 1
+    } else{
+      sort_check  <- function(x, y){
+        ifelse(length(x) == 0 | length(y) == 0, FALSE, 
+               ifelse(length(x) != length(y), FALSE, all(sort(x) == sort(y))))
+      }
+
+      log_logical <- as.integer(mapply(sort_check, x = out$col_name.x, y = out$col_name.y)) + 1
+    }
+    out$equal     <- c('\u2718', '\u2714')[log_logical]
+    
+    
+    s1$col_name <- lapply(s1$col_name, function(v) tibble(position = names(v), col_name = v))
+    s2$col_name <- lapply(s2$col_name, function(v) tibble(position = names(v), col_name = v))
+    
+    a <- s1 %>% select(type, col_name) %>% unnest(col_name)
+    b <- s2 %>% select(type, col_name) %>% unnest(col_name)
+    
+    drudge <- full_join(a, b, by = 'col_name') %>%
+      mutate(type_diff = case_when(
+        type.x != type.y ~ paste0(
+          '\u2718 ',
+          df_names$df1, '::', col_name, ' ~ ',
+          type.x, '() <!> ', df_names$df2, '::', 
+          col_name, ' ~ ', type.y, '()'
+        )
+      )) %>%
+      mutate(missing_col = case_when(
+        is.na(type.x) ~ paste0(
+          '\u2718 ', df_names$df2, '::',
+          col_name, ' ~ ', type.y, 
+          '()  missing from data::', 
+          df_names$df1
+        ), 
+        is.na(type.y) ~ paste0(
+          '\u2718 ', df_names$df1, '::',
+          col_name, ' ~ ', type.x, 
+          '()  missing from data::', 
+          df_names$df2
+        )
+      )) 
+    
+    drudge_nest <- bind_rows(
+      drudge %>% select(type = type.x, comment = type_diff), 
+      drudge %>% select(type = type.y, comment = type_diff), 
+      drudge %>% select(type = type.x, comment = missing_col), 
+      drudge %>% select(type = type.y, comment = missing_col)
+    ) %>%
+      filter(!is.na(comment), !is.na(type)) %>%
+      nest(issues = comment)
+    
+    out <- out %>%
+      left_join(drudge_nest, by = 'type') %>% 
+      select(-matches('col_name')) %>% 
       replace(is.na(.), 0)
+    
     # attach attributes required for plotting
     attr(out, "type") <- list(method = "types", 2)
     attr(out, "df_names") <- df_names
   }
   return(out)
 }
-
-
 
 
 
